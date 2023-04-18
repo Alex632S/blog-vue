51dbd1a378cc52d4227c9ffdc7a7e0369efb5f86
Alexandr Smotrov <98663387+Alex632S@users.noreply.github.com>
2023-04-19 02:32:32 +0300

    change layout

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,48 @@
 <template>
   <v-card>
     <v-layout>
-      <v-app-bar color="surface-variant" title="Application bar"></v-app-bar>
+      <v-system-bar color="grey-darken-3"></v-system-bar>
 
-      <v-navigation-drawer color="surface">
-        <v-list>
-          <v-list-item title="Drawer left"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <v-navigation-drawer
+        color="grey-darken-2"
+        permanent
+        width="72"
+      ></v-navigation-drawer>
 
-      <v-navigation-drawer color="primary" location="right">
-        <v-list>
-          <v-list-item title="Drawer right"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <v-navigation-drawer
+        color="grey-darken-1"
+        permanent
+        width="150"
+      ></v-navigation-drawer>
 
-      <v-main style="min-height: 100vh"></v-main>
+      <v-app-bar
+        color="grey"
+        height="48"
+        flat
+      ></v-app-bar>
+
+      <v-navigation-drawer
+        color="grey-lighten-1"
+        location="right"
+        permanent
+        width="150"
+      ></v-navigation-drawer>
+
+      <v-app-bar
+        color="grey-lighten-2"
+        flat
+        height="48"
+        location="bottom"
+      ></v-app-bar>
+
+      <v-main>
+        <v-container
+          class="d-flex justify-center align-center text-h5"
+          style="min-height: 300px;"
+        >
+          Content
+        </v-container>
+      </v-main>
     </v-layout>
   </v-card>
 </template>
